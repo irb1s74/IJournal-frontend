77737b29ec8d876cb792b147abc7b921893d7025
irb1s <kolyirb1s@gmail.com>
2022-04-01 17:10:51 +0500

    Commit

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import { IoMenu, IoWine } from 'react-icons/io5';
+import { IoMenu, IoFlameSharp } from 'react-icons/io5';
 import Search from '../UI/Search/Search';
 import { IAppSetMenu } from '../../store/reducers/appReducer/types';
 import HeaderMenu from './widget/Menu';
@@ -40,7 +40,7 @@ export const Header: FC<{
             <Typography variant='h5' component='div' noWrap>
               VIBE
             </Typography>
-            <IoWine size={24} color='#DA4A5E' />
+            <IoFlameSharp size={24} color='#DA4A5E' />
           </Box>
           <Search />
           <Button variant='contained' color='secondary'>
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,6 +1,5 @@
 /// <reference types="react-scripts" />
 interface Window {
-  ethereum: any;
   __REDUX_DEVTOOLS_EXTENSION__: any;
 }
 
